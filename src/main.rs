cd587ee9608c314141354672b6bab12cde563f92
Nic Pearson <nic.s.pearson@gmail.com>
2021-02-06 10:51:11 +0100

    feat: add rest endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello World!");
+#[macro_use]
+extern crate actix_web;
+
+use std::{env, io};
+
+use actix_web::{middleware, App, HttpServer};
+
+mod constants;
+mod like;
+mod response;
+mod tweet;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            // enable logger - always register actix-web Logger middleware last
+            .wrap(middleware::Logger::default())
+            // register HTTP requests handlers
+            .service(tweet::list)
+            .service(tweet::get)
+            .service(tweet::create)
+            .service(tweet::delete)
+            .service(like::list)
+            .service(like::plus_one)
+            .service(like::minus_one)
+    })
+    .bind("0.0.0.0:3000")?
+    .run()
+    .await
 }
